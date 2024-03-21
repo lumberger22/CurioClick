d685a932190a3bb66e184019e9a1762b9e8d8edd
lucasvt22 <lucas22@vt.edu>
2024-03-21 09:42:00 -0400

    Add access key variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 
+const ACCESS_KEY = import.meta.env.MOVIE_API_ACCESS_KEY;
+
 function App() {
 
   return (
     <>
-      
+      <p>Hello</p>
     </>
   )
 }
