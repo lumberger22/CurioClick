278998c3d9d924b41fe2b80ce165179721bb2b21
lucasvt22 <lucas22@vt.edu>
2024-03-22 12:44:19 -0400

    Create previous movies gallery component

---
+++ src/Components/Gallery.jsx
@@ -0,0 +1,37 @@
+import '../App.css'
+
+function Gallery({ movies }) {
+
+    const getImage = (movie) => {
+        let path = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
+        return path;
+    }
+
+    return(
+        <>
+            <div>
+                <h2>Previous Movies</h2>
+                <div className="movie-container">
+                    {movies && movies.length > 1 ? (
+                        movies.slice(1).map((movie, index) => (
+
+                            <img
+                                className="gallery-image"
+                                src={getImage(movie)}
+                                alt="Undefined image from query"
+                                width="50"
+                            />
+                        )
+                        )
+                    ) : (
+                        <div>
+                            <h3>No Movies Discovered!</h3>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Gallery
